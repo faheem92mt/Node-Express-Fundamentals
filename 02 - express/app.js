87f12aaebf99e5b40a397cf7b73c1901a55529dd
faheem92mt <faheemibnhabib@gmail.com>
2024-02-23 00:06:49 +0800

    add

--- 02 - express/app.js
+++ 02 - express/app.js
@@ -7,10 +7,16 @@ const http = require('http');
 // the method 'createServer' takes a callback which is going to be invoked everytime 
 // the user hits our server...
 const server = http.createServer( (req,res) => {
+    
     console.log('user hit the server');
+
+    res.writeHead(200, {'content-type' : 'text/html'})
+
     // 'res' is the response object to respond to the browser in a meaningful manner
+    res.write('<h1>home page </h1>');
     // 'end' means the communication is over/finished
-    res.end('homepage');
+    res.end();
+    
 })
 
 server.listen(5000);
