c186816db66077036e9c4e3650d0eb7e8f0803a5
faheem92mt <faheemibnhabib@gmail.com>
2024-03-12 00:22:19 +0800

    comment

--- 02 - express/final/02-http-app.js
+++ 02 - express/final/02-http-app.js
@@ -1,52 +1,71 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+// console.log('Express Tutorial')
+
+// creating an object called http
+const http = require('http');
+const {readFileSync} = require('fs');
 
 // get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
-const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  console.log(url)
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // styles
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' })
-    res.write(homeStyles)
-    res.end()
-  }
-  // image/logo
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' })
-    res.write(homeImage)
-    res.end()
-  }
-  // logic
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' })
-    res.write(homeLogic)
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
+
+// on the object 'http', we have a method called 'createServer' 
+// the method 'createServer' takes a callback which is going to be invoked everytime 
+// the user hits our server...
+const server = http.createServer( (req,res) => {
+    // console.log('user hit the server');
+
+    const url = req.url;
+    console.log(url);
+
+    if (url === '/') {
+        res.writeHead(200, {'content-type' : 'text/html'})
+
+        // 'res' is the response object to respond to the browser in a meaningful manner
+        res.write(homePage);
+        // 'end' means the communication is over/finished
+        // somewhere saw send ; but here end
+        res.end();
+    }
+    else if (url === '/about') {
+        
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1>About Page</h1>');
+        res.end();
+
+    }
+    else if (url === '/styles.css') {
+        
+        res.writeHead(200, {'content-type': 'text/css'});
+        res.write(homeStyles);
+        res.end();
+
+    }
+    else if (url === '/logo.svg') {
+        
+        res.writeHead(200, {'content-type': 'image/svg+xml'});
+        res.write(homeImage);
+        res.end();
+
+    }
+    else if (url === '/browser-app.js') {
+        
+        res.writeHead(200, {'content-type': 'text/javascript'});
+        res.write(homeLogic);
+        res.end();
+
+    }
+    else {
+
+        res.writeHead(404, {'content-type':'text/html'});
+        res.write('<h1>Error 404 Not Found</h1>');
+        res.end();
+    }
+
+    
+    
 })
 
-server.listen(5000)
+server.listen(5000);
