041d214e3afe239af806538df61b85b54abd47ab
faheem92mt <faheemibnhabib@gmail.com>
2024-03-15 00:22:45 +0800

    comment

--- 02 - express/final/03-express-basics.js
+++ 02 - express/final/03-express-basics.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
+    console.log('user hit the resource');
+    res.status(200).send('Home Page');
 })
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+app.get('/about', (req,res) => {
+    res.status(200).send('About Page')
 })
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
+app.all('*', (req,res) => {
+    res.status(404).send('<h1>resource not found</h1>');
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+    console.log("server is listening on port 5000");
 })
+
+// http verbs - what the user is trying to do
+// GET = Read Data
+// POST = Insert Data
+// PUT = Update Data
+// DELETE = Delete Data
 
 // app.get
 // app.post
