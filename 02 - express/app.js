2aa99b6f6e75e949f4b18ad71fb8ae5d492f9853
faheem92mt <faheemibnhabib@gmail.com>
2024-03-16 00:07:16 +0800

    comment

--- 02 - express/app.js
+++ 02 - express/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static('./public'))
 // so what is static asset? we need to understand this first...
 // its a file that server doesnt have to change it
-// so just dump all the assets like images and such in the 'public' folder
+// so just dump all the assets like images (jpg) or css style file or javascript file and such in the 'public' folder
 // thanks to express, we dont have to set up the paths, the mime types, status codes
 
 app.get('/', (req,res) => {
