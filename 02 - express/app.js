6b41f1d7b842ba1296360c10785b7d0fb285dd82
faheem92mt <faheemibnhabib@gmail.com>
2024-04-18 22:23:04 +0800

    comment

--- 02 - express/app.js
+++ 02 - express/app.js
@@ -1,19 +1,15 @@
-const express = require('express')
-const path = require('path')
+// The Most Basic JSON Setup
+const express = require('express');
+const app = express();
 
-const app = express()
+// importing an object which consists of multiple things (2 arrays -> products, people) from 'data.js'
+// module.exports = { products, people }
+const {products} = require('./data');
 
-// setup static and middleware
-// app.use(express.static('./public'))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get('/', (req,res) => {
+    res.json(products);
 })
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
-
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
+app.listen(5000, (req,res) => {
+    console.log('server is listening on port 5000');
 })
