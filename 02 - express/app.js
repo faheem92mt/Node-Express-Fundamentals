d214f145364271e7de93db063e4d3a616f8b536a
faheem92mt <faheemibnhabib@gmail.com>
2024-04-30 23:17:15 +0800

    comment

--- 02 - express/app.js
+++ 02 - express/app.js
@@ -3,33 +3,47 @@ const app = express()
 
 let {people} = require('./data')
 
+
+
 // to use static assets - to start off the homepage at index.html
 app.use(express.static('./methods-public'))
 
-// parse form data - ???
+// parse form data - if we dont use this - then req.body will be undefined in /login
 app.use(express.urlencoded({ extended: false }))
 
+// parse json
+app.use(express.json())
 
 
 
+// ------------------------------------------------------------------
 // for regular form
 app.post('/login', (req, res) => {
-  const { name } = req.body
+  console.log(req.body);
+  const { blood } = req.body
 
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`)
+  if (blood) {
+    return res.status(200).send(`Welcome ${blood}`)
   }
 
   res.status(401).send('Please Provide Credentials')
 })
-
+// ------------------------------------------------------------------
 // for js form - this is from where - the js form fetches data
 app.get('/api/people', (req,res) => {
   res.status(200).json({success:true, data:people})
 })
+// ------------------------------------------------------------------
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
 
-app.post('/api/people', (req,res) => {
-  console.log("hello");
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
+  }
+  
+  res.status(201).json({ success: true, person: name })
 })
 
 
